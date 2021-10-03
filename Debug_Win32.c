74fd1ad9aeeb46c795058a3f6276d0d70003f702
frankrx41 <frankrx41@gmail.com>
2021-10-03 21:55:53 +0800

    Add Log and test example

--- Debug_Win32.c
+++ Debug_Win32.c
@@ -1,6 +1,7 @@
 #define PLATFORM_WIN32      1
 #include "CoreMini.h"
 
+#include "String.h"
 #include <Windows.h>
 
 void Engine_Debug_Break()
@@ -10,5 +11,12 @@ void Engine_Debug_Break()
 
 error Log(int32 type, const tchar* format, ...)
 {
+    String* string = String_New("", 512);
+    va_list ap;
+    va_start(ap, format);
+    String_FormatArgs(string, format, ap);
+    va_end(ap);
 
+    OutputDebugStringA(String_CStr(string));
+    return 0;
 }
--- main.c
+++ main.c
@@ -1,8 +1,52 @@
 #include "CoreMini.h"
 
+int32 main()
+{
+    Log(2, "error %s", "hello");
+    return 0;
+};
 
-int32 main(){
+// Log
+#if 0
+int32 main()
+{
+    Log(2, "error %s", "hello");
+    return 0;
+};
+#endif
 
+// Memory test
+#if 0
+#include "Memory.h"
 
+int32 main()
+{
+    Assert(true, "");
+    tptr ptr = MemNewSize("a", 256);
+    MemDel("a", ptr);
+    return 0;
+};
+#endif
+
+// String test
+#if 0
+#include "String.h"
+#include <stdio.h>
+
+int32 main()
+{
+
+    String* string = String_New("hello world", 0);
+    printf(String_CStr(string));
+    printf("\n");
+    String_Del(string);
+
+    for( int32 i=0; i<10; i++)
+    {
+        printf("%x\t", String_Crc("hello world"));
+        printf("%x\n", String_Crc("hello world2"));
+    }
     return 0;
 }
+
+#endif
