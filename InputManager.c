559a46c79f7baf32f27dfa1f67df21de3046c7fa
frankrx41 <frankrx41@gmail.com>
2021-10-10 21:10:37 +0800

    Add InputActionEvent

--- InputManager.c
+++ InputManager.c
@@ -7,8 +7,12 @@
 #include "InputManager.h"
 #include "EventManager.h"
 
+#include "Queue.h"
+#include "String.h"
+
 
 typedef struct KeyState KeyState;
+typedef struct InputActionEvent InputActionEvent;
 
 struct KeyState
 {
@@ -22,10 +26,36 @@ struct KeyState
     float   m_down_seconds;
 };
 
+struct InputActionEvent
+{
+    KeyId               m_key_id;
+    KeyActivateState    m_key_activate_state;
+    Event               m_event;
+};
+
+InputActionEvent* InputActionEvent_Create(const tchar* local_name, KeyId key_id, KeyActivateState key_activate_state, Event event)
+{
+    InputActionEvent* input_action_event = MemNew(local_name, InputActionEvent);
+    input_action_event->m_key_id                = key_id;
+    input_action_event->m_key_activate_state    = key_activate_state;
+    input_action_event->m_event                 = event;
+
+    return input_action_event;
+}
+
+void InputActionEvent_Destroy(InputActionEvent* input_action_event)
+{
+    MemDel(input_action_event);
+}
+
+////////////////////////////////////////////////////////////////////////////////
 struct InputManager
 {
-    KeyState    m_key_state[KeyId_Max];
+    KeyState    m_key_state[KeyId_Max - KeyId_Min];
     float       m_hold_need_seconds;
+    tptr        m_platform_data;
+    String*     m_local_name;
+    Queue(InputActionEvent*)*   m_input_action_event_queue;
 };
 
 bool InputManager_Key_IsPhysicsDown_Plat(KeyId key_id);
@@ -34,10 +64,10 @@ bool InputManager_Key_IsPhysicsDown_Plat(KeyId key_id);
 InputManager* InputManager_Create(const tchar* local_name)
 {
     InputManager* input_manager = MemNew(local_name, InputManager);
-    for(KeyId key_id=0; key_id<KeyId_Max; key_id++)
+    for(uint32 i=0, max_i = ARRAY_SIZE(input_manager->m_key_state); i<max_i; i++)
     {
-        KeyState* key_state = &input_manager->m_key_state[key_id];
-        key_state->m_key_id         = key_id;
+        KeyState* key_state = &input_manager->m_key_state[i];
+        key_state->m_key_id         = KeyId_Min + i;
         key_state->m_is_down        = false;
         key_state->m_is_hold        = false;
         key_state->m_is_toggle      = false;
@@ -46,13 +76,42 @@ InputManager* InputManager_Create(const tchar* local_name)
         key_state->m_is_up_once     = false;
         key_state->m_down_seconds   = 0.f;
     }
-    input_manager->m_hold_need_seconds = 1.f;
+
+    input_manager->m_input_action_event_queue = Queue_Create(local_name, InputActionEvent*);
+    input_manager->m_local_name         = String_New(local_name, local_name, true);
+    input_manager->m_hold_need_seconds  = 1.f;
     return input_manager;
 }
 
 void InputManager_Destroy(InputManager* input_manager)
 {
+    Queue_Destroy(input_manager->m_input_action_event_queue, InputActionEvent_Destroy);
+    String_Del(input_manager->m_local_name);
+
     MemDel(input_manager);
+}
+
+void InputManager_InputActionEvent_Add(InputManager* input_manager, KeyId key_id, KeyActivateState key_activate_state, Event event)
+{
+    InputActionEvent* input_action_event = InputActionEvent_Create(String_CStr(input_manager->m_local_name), key_id, key_activate_state, event);
+
+    Queue_Push(InputActionEvent*, String_CStr(input_manager->m_local_name), input_manager->m_input_action_event_queue, input_action_event);
+}
+
+static bool CallBack_Find_InputActionEvent(InputActionEvent* input_action_event, Event event)
+{
+    return input_action_event->m_event == event;
+}
+
+void InputManager_InputActionEvent_Del(InputManager* input_manager, Event event)
+{
+    InputActionEvent* input_action_event = Queue_RemoveFindFirst(InputActionEvent*)(input_manager->m_input_action_event_queue, CallBack_Find_InputActionEvent, (const tptr)event);
+    InputActionEvent_Destroy(input_action_event);
+}
+
+void InputManager_InputActionEvent_Clear(InputManager* input_manager)
+{
+    Queue_Clear(input_manager->m_input_action_event_queue, InputActionEvent_Destroy);
 }
 
 void InputManager_Event_Send(InputManager* input_manager, const tchar* local_name)
--- InputManager.h
+++ InputManager.h
@@ -3,7 +3,26 @@
 typedef struct InputManager InputManager;
 typedef enum KeyId KeyId;
 
+typedef enum KeyActivateState KeyActivateState;
+
+enum KeyActivateState
+{
+    Key_Hold,
+    Key_Down,
+    Key_Up,
+    Key_UpOnce,
+    Key_DownOnce,
+    Key_Toggle,
+};
+
 void    InputManager_Keys_UpdateState   (InputManager* input_manager, float delta_second);
 void    InputManager_Event_Send         (InputManager* input_manager, const tchar* local_name);
 
+void    InputManager_InputActionEvent_Add   (InputManager* input_manager, KeyId key_id, KeyActivateState key_activate_state, Event event);
+void    InputManager_InputActionEvent_Del   (InputManager* input_manager, Event event);
+void    InputManager_InputActionEvent_Clear (InputManager* input_manager);
+
+
 InputManager* InputManager_GetInstance  ();
+
+COMPILE_TIME_REQUIRE(EVENT);
