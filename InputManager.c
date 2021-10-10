cf1d2a5f67cb113931ff4f550a37113671029ddf
frankrx41 <frankrx41@gmail.com>
2021-10-10 21:59:38 +0800

    Rename KeyState -> KeyInfo

--- InputManager.c
+++ InputManager.c
@@ -11,10 +11,10 @@
 #include "String.h"
 
 
-typedef struct KeyState KeyState;
+typedef struct KeyInfo KeyInfo;
 typedef struct InputActionEvent InputActionEvent;
 
-struct KeyState
+struct KeyInfo
 {
     KeyId   m_key_id;
     bool    m_is_down           :1;
@@ -51,7 +51,7 @@ void InputActionEvent_Destroy(InputActionEvent* input_action_event)
 ////////////////////////////////////////////////////////////////////////////////
 struct InputManager
 {
-    KeyState    m_key_state[KeyId_Max - KeyId_Min];
+    KeyInfo     m_key_info[KeyId_Max - KeyId_Min];
     float       m_hold_need_seconds;
     tptr        m_platform_data;
     String*     m_local_name;
@@ -64,17 +64,17 @@ bool InputManager_Key_IsPhysicsDown_Plat(KeyId key_id);
 InputManager* InputManager_Create(const tchar* local_name)
 {
     InputManager* input_manager = MemNew(local_name, InputManager);
-    for(uint32 i=0, max_i = ARRAY_SIZE(input_manager->m_key_state); i<max_i; i++)
+    for(uint32 i=0, max_i = ARRAY_SIZE(input_manager->m_key_info); i<max_i; i++)
     {
-        KeyState* key_state = &input_manager->m_key_state[i];
-        key_state->m_key_id         = KeyId_Min + i;
-        key_state->m_is_down        = false;
-        key_state->m_is_hold        = false;
-        key_state->m_is_toggle      = false;
-        key_state->m_is_down_once   = false;
-        key_state->m_is_down_before = false;
-        key_state->m_is_up_once     = false;
-        key_state->m_down_seconds   = 0.f;
+        KeyInfo* key_info = &input_manager->m_key_info[i];
+        key_info->m_key_id          = KeyId_Min + i;
+        key_info->m_is_down         = false;
+        key_info->m_is_hold         = false;
+        key_info->m_is_toggle       = false;
+        key_info->m_is_down_once    = false;
+        key_info->m_is_down_before  = false;
+        key_info->m_is_up_once      = false;
+        key_info->m_down_seconds    = 0.f;
     }
 
     input_manager->m_input_action_event_queue = Queue_Create(local_name, InputActionEvent*);
@@ -120,7 +120,7 @@ void InputManager_Event_Send(InputManager* input_manager, const tchar* local_nam
 {
     for(KeyId key_id=0; key_id<KeyId_Max; key_id++)
     {
-        KeyState* key_state = &input_manager->m_key_state[key_id];
+        KeyInfo* key_state = &input_manager->m_key_info[key_id];
 
         if( key_state->m_is_hold )
         {
@@ -150,49 +150,49 @@ void InputManager_Keys_UpdateState(InputManager* input_manager, float delta_seco
 {
     for(KeyId key_id=KeyId_Min; key_id<KeyId_Max; key_id++)
     {
-        KeyState* key_state = &input_manager->m_key_state[key_id];
+        KeyInfo* key_info = &input_manager->m_key_info[key_id];
 
-        key_state->m_is_down_before = key_state->m_is_down;
+        key_info->m_is_down_before = key_info->m_is_down;
 
         if( InputManager_Key_IsPhysicsDown_Plat(key_id) )
         {
-            key_state->m_is_up_once = false;
+            key_info->m_is_up_once = false;
 
-            if( key_state->m_down_seconds == 0 )
+            if( key_info->m_down_seconds == 0 )
             {
-                key_state->m_is_down_once   = true;
-                key_state->m_is_down        = true;
+                key_info->m_is_down_once   = true;
+                key_info->m_is_down        = true;
 
-                if( key_state->m_is_toggle )
+                if( key_info->m_is_toggle )
                 {
-                    key_state->m_is_toggle = false;
+                    key_info->m_is_toggle = false;
                 }
                 else
                 {
-                    key_state->m_is_toggle = true;
+                    key_info->m_is_toggle = true;
                 }
             }
 
-            float down_second_time = key_state->m_down_seconds;
+            float down_second_time = key_info->m_down_seconds;
             if( down_second_time > input_manager->m_hold_need_seconds )
             {
-                key_state->m_is_hold = true;
+                key_info->m_is_hold = true;
             }
             else
             {
-                key_state->m_down_seconds += delta_second;
+                key_info->m_down_seconds += delta_second;
             }
         }
         else
         {
-            key_state->m_is_down        = false;
-            key_state->m_is_down_once   = false;
-            key_state->m_is_hold        = false;
-            key_state->m_down_seconds   = 0.f;
+            key_info->m_is_down        = false;
+            key_info->m_is_down_once   = false;
+            key_info->m_is_hold        = false;
+            key_info->m_down_seconds   = 0.f;
 
-            if( key_state->m_is_down_before )
+            if( key_info->m_is_down_before )
             {
-                key_state->m_is_up_once = true;
+                key_info->m_is_up_once = true;
             }
         }
     }
